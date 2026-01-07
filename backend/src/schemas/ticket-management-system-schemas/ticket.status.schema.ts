16e3d4573ddc1a794a6fa6914f478edc3855c347
shahzaib <zaib96189@gmail.com>
2026-01-07 13:51:18 +0500

    add ticket status

--- backend/src/schemas/ticket-management-system-schemas/ticket.status.schema.ts
+++ backend/src/schemas/ticket-management-system-schemas/ticket.status.schema.ts
@@ -1,26 +1,24 @@
 import { z, ZodObject } from "zod";
 import { SchemaDefinition, Types } from "mongoose";
-import { ITicketStatus } from '../../../../common/Ticket-management-system/ITicketStatus.interface'
+import { ITicketStatus } from "../../../../common/Ticket-management-system/ITicketStatus.interface";
 import { commonSchema, commonSchemaValidation } from "../shared/common.schema";
 
-
-export const ticketStatusSchema: SchemaDefinition<ITicketStatus<Types.ObjectId>> = {
-    ...commonSchema,
-    code: { type: String, required: true, unique: true },
-    label: { type: String },
-    is_Terminal: { type: Boolean, required: true }
-
-}
+export const ticketStatusSchema: SchemaDefinition<
+  ITicketStatus<Types.ObjectId>
+> = {
+  ...commonSchema,
+  code: { type: String, required: true, unique: true },
+  label: { type: String },
+  is_Terminal: { type: Boolean, required: true },
+};
 
 export const ticketStatusSchemaValidation = z.object({
-    ...commonSchemaValidation,
-    code: z
-        .string()
-        .trim()
-        .min(1, "Enter code please")
-        .transform(v => v.toUpperCase()),
-    label: z.string().optional(),
-    is_terminal: z.boolean().default(false)
+  ...commonSchemaValidation,
+  code: z
+    .string()
+    .trim()
+    .min(1, "Enter code please")
+    .transform((v) => v.toUpperCase()),
+  label: z.string().optional(),
+  is_Terminal: z.boolean().default(false),
 });
-
-
--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,9 +4,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
 
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Alias for common folder
-    config.resolve.alias['@common'] = path.resolve(__dirname, '../common');
+  turbopack: {},
+
+  webpack: (config) => {
+    config.resolve.alias["@common"] = path.resolve(__dirname, "../common");
     return config;
   },
 };
