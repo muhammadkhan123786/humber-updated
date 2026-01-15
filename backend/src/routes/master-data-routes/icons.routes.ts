9634a6b793270e65c29962a6a19bbef20c66c1b4
muhammadkhan123786 <muhammadkhan123829@gmail.com>
2026-01-15 17:00:36 +0500

    Icon Api changed to upload icon

--- backend/src/routes/master-data-routes/icons.routes.ts
+++ backend/src/routes/master-data-routes/icons.routes.ts
@@ -3,7 +3,16 @@ import { GenericService } from "../../services/generic.crud.services";
 import { Icons, iconDoc } from "../../models/master-data-models/icons.models";
 import { iconSchemaValidation } from "../../schemas/master-data/icons.schema";
 import { AdvancedGenericController } from "../../controllers/GenericController";
+import { createUploader } from "../../config/multer";
+import { mapUploadedFilesToBody } from "../../middleware/mapUploadedFiles";
 
+const iconUpload = createUploader([
+    {
+        name: "icon",
+        maxCount: 1,
+        mimeTypes: ["image/jpeg", "image/png"],
+    }
+]);
 const iconsRouter = Router();
 
 const iconServices = new GenericService<iconDoc>(Icons);
@@ -17,8 +26,8 @@ const iconsController = new AdvancedGenericController({
 
 iconsRouter.get("/", iconsController.getAll);
 iconsRouter.get("/:id", iconsController.getById);
-iconsRouter.post("/", iconsController.create);
-iconsRouter.put("/:id", iconsController.update);
+iconsRouter.post("/", iconUpload, mapUploadedFilesToBody(), iconsController.create);
+iconsRouter.put("/:id", iconUpload, mapUploadedFilesToBody(), iconsController.update);
 iconsRouter.delete("/:id", iconsController.delete);
 
 export default iconsRouter;
