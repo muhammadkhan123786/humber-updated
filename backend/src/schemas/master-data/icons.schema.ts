7af17b51671b0cf5d11fbe596d0a592d8fed1827
muhammadkhan123786 <muhammadkhan123829@gmail.com>
2026-01-15 16:30:42 +0500

    Added Icons master data apis

--- backend/src/schemas/master-data/icons.schema.ts
+++ backend/src/schemas/master-data/icons.schema.ts
@@ -1,17 +1,19 @@
 import { z } from "zod";
 import { Types } from "mongoose";
 
-export const jobTitleSchema = {
+export const iconSchema = {
     userId: { type: Types.ObjectId, ref: "User", required: true },
-    jobTitleName: { type: String },
+    iconName: { type: String },
+    icon: { type: String },
     isActive: { type: Boolean, required: true, default: true },
     isDeleted: { type: Boolean, required: true, default: false },
     isDefault: { type: Boolean, required: true, default: false },
 }
 
-export const jobTitleSchemaValidation = z.object({
+export const iconSchemaValidation = z.object({
     userId: z.string().min(1, "userId is required"),  // will be converted to ObjectId
-    jobTitleName: z.string().min(1, "Job Title name is required"),
+    iconName: z.string().min(1, "Icon name is required"),
+    icon: z.string().min(1, "Icon is required."),
     isActive: z.boolean().optional(),
     isDeleted: z.boolean().optional(),
     isDefault: z.boolean().optional(),
