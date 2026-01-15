0a73601247d1b2b2d6a5ba381e03e7f97ac77dc0
muhammadkhan123786 <muhammadkhan123829@gmail.com>
2026-01-15 15:56:06 +0500

    Updated the master color schema for code

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -58,6 +58,7 @@ import businessTypeRouter from "./routes/suppliers/business.types.routes";
 import paymentMethodRouter from "./routes/suppliers/payment.method.routes";
 import pricingAgreementRouter from "./routes/suppliers/pricing.agreement.routes";
 import productServicesRouter from "./routes/suppliers/product.services.routes";
+import jobTitleRouter from "./routes/master-data-routes/job.titles.routes";
 
 // Create express app
 const app: Application = express();
@@ -224,7 +225,9 @@ app.use(`${process.env.API_PREFIX}/job-types`, adminProtecter, jobTypesRouter);
 //vehicle register api 13-01-2026
 app.use(`${process.env.API_PREFIX}/customer-vehicle-register`, adminProtecter, customerVehicleRouter);
 
+
 //ticket generate routes 14-01-2026
+
 app.use(`${process.env.API_PREFIX}/customer-tickets`, adminProtecter, customerTicketBaseRouter);
 
 app.use(`${process.env.API_PREFIX}/business-types`, adminProtecter, businessTypeRouter);
@@ -234,6 +237,10 @@ app.use(`${process.env.API_PREFIX}/payment-method`, adminProtecter, paymentMetho
 app.use(`${process.env.API_PREFIX}/pricing-agreement`, adminProtecter, pricingAgreementRouter);
 
 app.use(`${process.env.API_PREFIX}/product-services`, adminProtecter, productServicesRouter);
+
+// 15-01-2026 Muhammad Imran 
+app.use(`${process.env.API_PREFIX}/job-titles`, adminProtecter, jobTitleRouter);
+
 
 //Muhammad Imran code ended here.
 
--- backend/src/schemas/master-data/job.titles.schema.ts
+++ backend/src/schemas/master-data/job.titles.schema.ts
@@ -1,20 +1,19 @@
 import { z } from "zod";
-import { Types } from "mongoose";
+import { SchemaDefinition, Types } from "mongoose";
+import { IJobTitles } from '../../../../common/suppliers/IJob.titles';
 
 
-export const colorSchema = {
+export const jobTitleSchema: SchemaDefinition<IJobTitles<Types.ObjectId>> = {
     userId: { type: Types.ObjectId, ref: "User", required: true },
-    colorName: { type: String, required: true },
-    colorCode: { type: String, required: true },
+    jobTitleName: { type: String },
     isActive: { type: Boolean, required: true, default: true },
     isDeleted: { type: Boolean, required: true, default: false },
     isDefault: { type: Boolean, required: true, default: false },
 }
 
-export const colorSchemaValidation = z.object({
+export const jobTitleSchemaValidation = z.object({
     userId: z.string().min(1, "userId is required"),  // will be converted to ObjectId
-    colorName: z.string().min(1, "Color name is required"),
-    colorCode: z.string().min(1, "Color code is required."),
+    jobTitleName: z.string().min(1, "Job Title name is required"),
     isActive: z.boolean().optional(),
     isDeleted: z.boolean().optional(),
     isDefault: z.boolean().optional(),
