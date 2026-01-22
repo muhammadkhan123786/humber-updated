7f8563af07bcdb42bb13abd24ab66a6e87edb3da
muhammadkhan123786 <muhammadkhan123829@gmail.com>
2026-01-22 11:47:45 +0500

    Updated permissions

--- backend/src/schemas/master-data/contract.types.schema.ts
+++ backend/src/schemas/master-data/contract.types.schema.ts
@@ -1,16 +1,12 @@
 import { z } from "zod";
-import { Types } from "mongoose";
 import { commonSchema, commonSchemaValidation } from "../shared/common.schema";
-import { commonCustomerValidationSchema } from "../corporate.customer.schema";
 
 export const contractTypeSchema = {
-  userId: { type: Types.ObjectId, ref: "User", required: true },
   contractType: { type: String },
   ...commonSchema,
 };
 
 export const contractTypeSchemaValidation = z.object({
-  userId: z.string().min(1, "userId is required"), // will be converted to ObjectId
   contractType: z.string().min(1, "Contract type name is required"),
   ...commonSchemaValidation,
 });
