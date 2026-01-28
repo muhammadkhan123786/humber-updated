84ee6f91a37bb28c7868cab85aa402debbfeed0d
Attiq-79 <70988@gcslahore.edu.pk>
2026-01-28 20:08:04 +0500

    integrate forget password api

---
+++ frontend/src/app/auth/forget-password/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ForgetPassword from './ForgetPassword'
+
+const page = () => {
+  return (
+    <div>
+     <ForgetPassword />
+    </div>
+  )
+}
+
+export default page
