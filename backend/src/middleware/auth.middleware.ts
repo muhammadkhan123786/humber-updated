c28eb1c3d9fc9bba4280c388f810cbfd5a4a478f
muhammadkhan123786 <muhammadkhan123829@gmail.com>
2026-02-03 10:15:15 +0500

    Update password api end points

--- backend/src/middleware/auth.middleware.ts
+++ backend/src/middleware/auth.middleware.ts
@@ -174,3 +174,39 @@ export const driverProtecter = (
         return; // ✅ return void
     }
 };
+
+
+//check user is valid logged in. 
+export const generalProtecter = (
+    req: AuthRequest,
+    res: Response,
+    next: NextFunction
+) => {
+    let token: string | undefined;
+
+    if (
+        req.headers.authorization &&
+        req.headers.authorization.startsWith("Bearer ")
+    ) {
+        token = req.headers.authorization.split(" ")[1];
+    }
+
+    if (!token) {
+        return res.status(401).json({ message: "Not authorized, token missing" });
+    }
+
+    try {
+        const decoded = jwt.verify(
+            token,
+            process.env.JWT_SECRET as string
+        ) as { id: string; role: Roles };
+
+        // ✅ Attach ONLY token data
+        req.user = { id: decoded.id };
+        req.role = decoded.role;
+
+        next();
+    } catch (error) {
+        return res.status(401).json({ message: "Not authorized, token invalid" });
+    }
+};
