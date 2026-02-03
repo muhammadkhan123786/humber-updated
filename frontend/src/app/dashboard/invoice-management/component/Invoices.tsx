cb8df3597412877c4cbb13eeddc3fd6ef3958668
shahzaib <zaib96189@gmail.com>
2026-02-03 12:50:20 +0500

    update invoice

---
+++ frontend/src/app/dashboard/invoice-management/component/Invoices.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import InvoiceHeader from "./InvoiceHeader";
+import DashboardStats from "./StatCard";
+
+const Invoices = () => {
+  return (
+    <div>
+      <InvoiceHeader />
+      <DashboardStats />
+    </div>
+  );
+};
+
+export default Invoices;
