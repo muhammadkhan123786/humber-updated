9c0bcf1a18f3886519acf38f22d3d08cb3b75dec
muhammadkhan123786 <muhammadkhan123829@gmail.com>
2026-02-12 17:06:06 +0500

    Technician dashboard jobs statistics

--- backend/src/routes/technician-dashboard/technician-jobs/technician.jobs.routes.ts
+++ backend/src/routes/technician-dashboard/technician-jobs/technician.jobs.routes.ts
@@ -2,9 +2,11 @@
 import { Request, Response, NextFunction, Router } from 'express';
 import { TechnicianAuthRequest } from '../../../middleware/auth.middleware';
 import { technicianDashboardJobsController } from '../../../controllers/technician-dashboard-controllers/technician-dashboard-jobs/technician.dashboard.jobs';
+import { technicianDashboardJobsStatisticsController } from '../../../controllers/technician-job-statistics/technician.jobs.statistics.controller';
 
 const technicianDashboardJobsRouter = Router();
 
 technicianDashboardJobsRouter.get('/', technicianDashboardJobsController)
+technicianDashboardJobsRouter.get('/technician-dashboard-jobs-statistics', technicianDashboardJobsStatisticsController)
 
 export default technicianDashboardJobsRouter;
