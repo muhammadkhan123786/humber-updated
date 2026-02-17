3f80e07434a25ec4b9fdc34aa644be2e49fbe94a
Attiq-79 <70988@gcslahore.edu.pk>
2026-02-17 14:36:35 +0500

    create route bar

--- frontend/src/app/dashboard/techniciandashboard/components/TechnicianDashboard.tsx
+++ frontend/src/app/dashboard/techniciandashboard/components/TechnicianDashboard.tsx
@@ -1,13 +1,16 @@
 "use client";
 import React from 'react'
 import MainBar from './MainBar';
+import RouteBar from './RouteBar';
 import StatsOverview from './StatsOverview';
 
 const TechnicianDashboard = () => {
   return (
     <div>
       <MainBar />
+     
       <StatsOverview />
+       <RouteBar />
     </div>
   )
 }
