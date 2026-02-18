4e40b7177e2bb389734a734bc16649324fc7d1cb
muhammadkhan123786 <muhammadkhan123829@gmail.com>
2026-02-18 13:35:45 +0500

    Quotation status updated

--- backend/src/schemas/ticket-quation-schemas/ticket.quation.status.schema.ts
+++ backend/src/schemas/ticket-quation-schemas/ticket.quation.status.schema.ts
@@ -4,11 +4,13 @@ import { commonSchema, commonSchemaValidation } from "../shared/common.schema";
 
 export const ticketQuationStatusSchema = {
     ticketQuationStatus: { type: String },
+    canChooseTechnician: { type: Boolean, default: false },
     ...commonSchema,
 };
 
 
 export const ticketQuationStatusValidation = z.object({
     ticketQuationStatus: z.string().min(1, "Please enter ticket quatation status name."),
+    canChooseTechnician: z.boolean().optional().default(false),
     ...commonSchemaValidation,
 });
