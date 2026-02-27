d59afa67577fa715fc01089496e401d0a7978a52
shahzaib <zaib96189@gmail.com>
2026-02-27 14:53:13 +0500

    update the code

---
+++ frontend/src/app/dashboard/availability/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AvailabilityClient from "./components/AvailabilityClient";
+
+const page = () => {
+  return (
+    <div>
+      <AvailabilityClient />
+    </div>
+  );
+};
+
+export default page;
