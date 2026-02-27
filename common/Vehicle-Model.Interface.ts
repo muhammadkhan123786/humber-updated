da907ee9c748dd43c7233c9bf52cb6a2dc58d697
muhammadkhan123786 <muhammadkhan123829@gmail.com>
2026-02-27 13:08:57 +0500

    update in vehicle model

--- common/Vehicle-Model.Interface.ts
+++ common/Vehicle-Model.Interface.ts
@@ -2,5 +2,6 @@ import { IBaseEntity } from './Base.Interface';
 export interface IVehicleModelInterface<TUserId = string, IBrandId = string> extends IBaseEntity<TUserId> {
     brandId: IBrandId
     modelName: string;
-
+    isCompany?:boolean;
+    isRider?:boolean;
 }
